f88c4a22a71d3f77593cb6a6430655a94b1ddf04
Stan Bondi <sdbondi@users.noreply.github.com>
2025-07-02 09:45:54 +0200

    remove init:true

--- crates/cli/src/cli/commands/create.rs
+++ crates/cli/src/cli/commands/create.rs
@@ -95,7 +95,6 @@ pub async fn handle(
             path: Some(template_path),
             ..TemplatePath::default()
         },
-        init: true, // Avoid workspace member processing since templates generate workspaces, not packages
         ..CargoGenerateArgs::default()
     };
     loading!(
