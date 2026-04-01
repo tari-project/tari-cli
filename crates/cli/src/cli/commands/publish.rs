cf1e33578f99c7e00ca288007564fd113a744a1b
Stan Bondi <sdbondi@users.noreply.github.com>
2026-04-01 14:39:22 +0200

    fmt

--- crates/cli/src/cli/commands/publish.rs
+++ crates/cli/src/cli/commands/publish.rs
@@ -155,7 +155,8 @@ pub async fn handle(config: Config, mut args: PublishArgs) -> anyhow::Result<()>
     let template = Template::Path { path: template_bin };
 
     // check balance and get max fee
-    let CheckBalanceResult { max_fee, binary_size } = publisher.check_balance_for_publish(&account, &template, None).await?;
+    let CheckBalanceResult { max_fee, binary_size } =
+        publisher.check_balance_for_publish(&account, &template, None).await?;
 
     if binary_size > MAX_WASM_SIZE {
         println!("⚠️ WASM binary size exceeded: {}", util::human_bytes(binary_size));
